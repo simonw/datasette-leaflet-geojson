f036e5fab92d4b7e5e20c330c7db099c41fe5285
Simon Willison <swillison@gmail.com>
2019-10-14 08:04:58 -0700

    Apply Black formatting

--- datasette_leaflet_geojson/__init__.py
+++ datasette_leaflet_geojson/__init__.py
@@ -3,15 +3,20 @@ from datasette import hookimpl
 
 @hookimpl
 def extra_css_urls():
-    return [{
-        "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css",
-        "sri": "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-    }]
+    return [
+        {
+            "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css",
+            "sri": "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==",
+        }
+    ]
 
 
 @hookimpl
 def extra_js_urls():
-    return [{
-        "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js",
-        "sri": "sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
-    }, "/-/static-plugins/datasette_leaflet_geojson/datasette-leaflet-geojson.js"]
+    return [
+        {
+            "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js",
+            "sri": "sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==",
+        },
+        "/-/static-plugins/datasette_leaflet_geojson/datasette-leaflet-geojson.js",
+    ]
