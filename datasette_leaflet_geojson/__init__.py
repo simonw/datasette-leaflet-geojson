db3f10ad9f90b359e96f6a831d78eea1b2438969
chris48s <chris48s@users.noreply.github.com>
2019-10-14 15:59:35 +0100

    Upgrade leaflet.js to 1.5.1 (#5)
    Thanks, @chris48s

--- datasette_leaflet_geojson/__init__.py
+++ datasette_leaflet_geojson/__init__.py
@@ -4,14 +4,14 @@ from datasette import hookimpl
 @hookimpl
 def extra_css_urls():
     return [{
-        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
-        "sri": "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+        "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css",
+        "sri": "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     }]
 
 
 @hookimpl
 def extra_js_urls():
     return [{
-        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
-        "sri": "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+        "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js",
+        "sri": "sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
     }, "/-/static-plugins/datasette_leaflet_geojson/datasette-leaflet-geojson.js"]
